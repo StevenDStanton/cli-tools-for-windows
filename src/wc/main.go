99a2d945e1afc145b1e587d0171253d9674a7f71
Steven Stanton <steven.stanton@gmail.com>
2024-06-05 21:13:55 -0400

    Added print Bytes

--- src/wc/main.go
+++ src/wc/main.go
@@ -7,28 +7,29 @@ import (
 )
 
 var (
-	// printBytes         = flag.Bool("bytes", false, "print the byte counts")
-	// printBytesShort    = flag.Bool("c", false, "print the byte counts")
-	// printChars         = flag.Bool("chars", false, "print the character counts")
-	// printCharsShort    = flag.Bool("m", false, "print the character counts")
-	// printLines         = flag.Bool("lines", false, "print the newline counts")
-	// printLinesShort    = flag.Bool("l", false, "print the newline counts")
-	// maxLineLength      = flag.Bool("max-line-length", false, "print the length of the longest line")
-	// maxLineLengthShort = flag.Bool("L", false, "print the length of the longest line")
-	// printWords         = flag.Bool("words", false, "print the word counts")
-	// printWordsShort    = flag.Bool("w", false, "print the word counts")
-	helpFlag    = flag.Bool("help", false, "display this help and exit")
-	versionFlag = flag.Bool("version", false, "output version information and exit")
-	aboutFlag   = flag.Bool("about", false, "display information about the program and exit")
+	printBytes         = flag.Bool("bytes", false, "print the byte counts")
+	printBytesShort    = flag.Bool("c", false, "print the byte counts")
+	printChars         = flag.Bool("chars", false, "print the character counts")
+	printCharsShort    = flag.Bool("m", false, "print the character counts")
+	printLines         = flag.Bool("lines", false, "print the newline counts")
+	printLinesShort    = flag.Bool("l", false, "print the newline counts")
+	maxLineLength      = flag.Bool("max-line-length", false, "print the length of the longest line")
+	maxLineLengthShort = flag.Bool("L", false, "print the length of the longest line")
+	printWords         = flag.Bool("words", false, "print the word counts")
+	printWordsShort    = flag.Bool("w", false, "print the word counts")
+	helpFlag           = flag.Bool("help", false, "display this help and exit")
+	versionFlag        = flag.Bool("version", false, "output version information and exit")
+	aboutFlag          = flag.Bool("about", false, "display information about the program and exit")
 )
 
 func main() {
-	const version = "0.0.3"
+	const version = "0.1.0"
+	flag.Parse()
+	args := flag.Args()
 	// charCount := 0
 	// newLineCount := 0
 	// maxLineLen := 0
 	// wordCount := 0
-	flag.Parse()
 
 	if *versionFlag {
 		fmt.Printf("go Version %s\nCopyright 2024 The Simple Dev\nLicense MIT - No Warranty\n\nWritten By Steven Stanton\nReverse Engineered by RTFM", version)
@@ -77,6 +78,42 @@ Part of my Linux Tools for Windows (ltfw) project.
 `
 		fmt.Println(about)
 		os.Exit(0)
+	}
+
+	filename := ""
+	fileData := []byte{}
+
+	if len(args) > 0 {
+		filename = args[0]
+	}
+
+	if filename != "" {
+		var err error
+		fileData, err = os.ReadFile(filename)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	if *printLines || *printLinesShort {
+
+	}
+
+	if *printWords || *printWordsShort {
+
+	}
+
+	if *printChars || *printCharsShort {
+
+	}
+
+	if *printBytes || *printBytesShort {
+		fmt.Println(len(fileData))
+	}
+
+	if *maxLineLength || *maxLineLengthShort {
+
 	}
 
 }
