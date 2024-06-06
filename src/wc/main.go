f90f913db5eecf652213c2bf696662ff1eb4b57f
Steven Stanton <steven.stanton@gmail.com>
2024-06-05 20:49:21 -0400

    Completed helpcommand

--- src/wc/main.go
+++ src/wc/main.go
@@ -35,7 +35,30 @@ func main() {
 	}
 
 	if *helpFlag {
+		help := `Usage: wc [OPTION]... [FILE]...
+Multiple Files: wc [OPTION]... --files0-from=F
 
+Prints new line, word, and byte counts for each FILE and a total line if more file is specified.
+A word is a non-zero length sequence of characters delimited by white space.
+
+When no FILE or when FILE is -, read from standard input.
+
+The options below may be used to select which counts are displayed. Always in the following order:
+newLine, word, character, byte, maximum line length.
+
+-c, --bytes            print the byte counts
+-m, --chars            print the character counts
+-l, --lines            print the newline counts
+-L, --max-line-length  print the length of the longest line
+-w, --words            print the word counts
+
+--files0-from=F        read input from the files specified by NUL-terminated names in file F; If F is - then read names from standard input
+
+--help                 display this help and exit
+--version              output version information and exit
+--about 			  display information about the program and exit`
+
+		fmt.Println(help)
 	}
 
 }
