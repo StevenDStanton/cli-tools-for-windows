27494962d98f3607da15bc9187c23a261a5aecc0
Steven Stanton <steven.stanton@gmail.com>
2024-06-16 17:11:11 -0400

    Setup cut application

--- src/cut/main.go
+++ src/cut/main.go
@@ -1,9 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"log"
+	"os"
+)
+
+var (
+	inputFileName = flag.String("i", "", "Input File Name")
+	developerFlag = flag.Bool("d", false, "In Development Flag to block accidental use")
+)
 
 func main() {
-	world := "Hello"
+	if !*developerFlag {
+		log.Fatalln("This program is in development and not ready for usage")
+	}
+}
 
-	fmt.Println(world)
+func readFile() {
+	file, err := os.Open(*inputFileName)
+	if err != nil {
+		log.Fatalf("Unable to read file, %v", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		chunk := scanner.Text()
+	}
+
 }
