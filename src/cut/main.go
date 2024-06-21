7c224be6760dde56bf98d3852bd0f08fe2c062f9
Steven Stanton <steven.stanton@gmail.com>
2024-06-20 20:11:51 -0400

    Fixing Deploy Issue

--- src/cut/main.go
+++ src/cut/main.go
@@ -7,27 +7,27 @@ import (
 	"os"
 )
 
-var (
-	inputFileName = flag.String("i", "", "Input File Name")
-	developerFlag = flag.Bool("d", false, "In Development Flag to block accidental use")
-)
+// var (
+// 	inputFileName = flag.String("i", "", "Input File Name")
+// 	developerFlag = flag.Bool("d", false, "In Development Flag to block accidental use")
+// )
 
 func main() {
-	if !*developerFlag {
-		log.Fatalln("This program is in development and not ready for usage")
-	}
+	// if !*developerFlag {
+	// 	log.Fatalln("This program is in development and not ready for usage")
+	// }
 }
 
-func readFile() {
-	file, err := os.Open(*inputFileName)
-	if err != nil {
-		log.Fatalf("Unable to read file, %v", err)
-	}
-	defer file.Close()
+// func readFile() {
+// 	file, err := os.Open(*inputFileName)
+// 	if err != nil {
+// 		log.Fatalf("Unable to read file, %v", err)
+// 	}
+// 	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		chunk := scanner.Text()
-	}
+// 	scanner := bufio.NewScanner(file)
+// 	for scanner.Scan() {
+// 		chunk := scanner.Text()
+// 	}
 
 }
